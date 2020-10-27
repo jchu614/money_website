5529058d43b651390e96d34f635639f9d72f559f
Jason Chu <jason.chu614@gmail.com>
2020-10-27 22:46:37 +0000

    app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res){
 app.use('/api/money', moneyRoutes);
 app.use('/api/account', userRoutes);
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + 'build', 'index.html'));
 });
