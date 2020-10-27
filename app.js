024cf86e64174a2ece0a2060f96872c49bd0b4b6
Jason Chu <jason.chu614@gmail.com>
2020-10-27 23:18:31 +0000

    app.js

--- app.js
+++ app.js
@@ -26,18 +26,18 @@ app.get('/', function(req, res){
 app.use('/api/money', moneyRoutes);
 app.use('/api/account', userRoutes);
 
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + 'build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'build', 'index.html'));
+});
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build/'));
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build/'));
 	
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 //PROCESS ENV
 app.listen(port, process.env.IP, function(){
