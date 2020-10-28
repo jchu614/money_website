d312017278f0ccfcf01af97f25fdaa4024d55e1d
Jason Chu <jason.chu614@gmail.com>
2020-10-28 16:59:46 +0000

    fixed package and app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'client/build')));
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
