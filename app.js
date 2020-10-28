1b6a306b286660c47e39a51542060e69f4d9bb00
Jason Chu <jason.chu614@gmail.com>
2020-10-28 19:04:29 +0000

    app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const path = require('path');
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, 'client/build')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 //REQUIRED ROUTES
